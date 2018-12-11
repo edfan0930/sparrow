583726f60acc2abae5316d59cb45d8d6871976f4
Ed <red@redde-Mac-mini.local>
2018-12-11 20:17:07 +0800

    add layout

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import Main from '@/views/Main';
 
 Vue.use(Router);
 
@@ -9,6 +10,26 @@ export default new Router({
       path: '/',
       name: 'Login',
       component: () => import('@/views/Login'),
+    },
+    {
+      path: '/main',
+      name: 'Main',
+      meta: {
+        icon: 'md-menu',
+        title: '多级菜单',
+      },
+      component: Main,
+      children: [
+        {
+          path: '/switch',
+          name: 'Switch',
+          meta: {
+            icon: 'md-funnel',
+            title: '二级-1',
+          },
+          component: () => import('@/views/user/switch'),
+        },
+      ],
     },
     {
       path: '*',
